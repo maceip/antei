7bd092c2a39ea2d12b17d253670cd57a51ec3a49
rpm <ryan.macarthur@gmail.com>
2023-04-16 02:08:54 +0900

    late

---
+++ pages/index.jsx
@@ -0,0 +1,357 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { Layout } from "@/components/layout"
+import { buttonVariants } from "@/components/ui/button"
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { useCallback, useEffect, useState } from 'react';
+import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import { ethers } from 'ethers';
+import {
+  getLoginUrl,
+  isSignInRedirect,
+  handleSignInRedirect,
+} from '@/lib/auth';
+import {
+  fetchPKPs,
+  mintPKP,
+  pollRequestUntilTerminalState,
+} from '@/lib/relay';
+import { useRouter } from 'next/router';
+
+
+
+const REDIRECT_URI =
+  process.env.NEXT_PUBLIC_REDIRECT_URI || 'http://localhost:3000';
+
+const Views = {
+  SIGN_IN: 'sign_in',
+  FETCHING: 'fetching',
+  FETCHED: 'fetched',
+  MINTING: 'minting',
+  MINTED: 'minted',
+  CREATING_SESSION: 'creating_session',
+  SESSION_CREATED: 'session_created',
+  ERROR: 'error',
+};
+
+
+export default function IndexPage() {
+const router = useRouter();
+const [view, setView] = useState(Views.SIGN_IN);
+const [error, setError] = useState();
+
+const [litNodeClient, setLitNodeClient] = useState();
+const [googleIdToken, setGoogleIdToken] = useState();
+const [pkps, setPKPs] = useState([]);
+const [currentPKP, setCurrentPKP] = useState();
+const [sessionSigs, setSessionSigs] = useState();
+
+const [message, setMessage] = useState('Free the web!');
+const [signature, setSignature] = useState(null);
+const [recoveredAddress, setRecoveredAddress] = useState(null);
+const [verified, setVerified] = useState(false);
+
+
+
+const handleRedirect = useCallback(async () => {
+  setView(Views.HANDLE_REDIRECT);
+  try {
+    // Get Google ID token from redirect callback
+    const googleIdToken = handleSignInRedirect(REDIRECT_URI);
+    setGoogleIdToken(googleIdToken);
+
+    // Fetch PKPs associated with Google account
+    setView(Views.FETCHING);
+    const pkps = await fetchGooglePKPs(googleIdToken);
+    if (pkps.length > 0) {
+      setPKPs(pkps);
+    }
+    setView(Views.FETCHED);
+  } catch (err) {
+    setError(err);
+    setView(Views.ERROR);
+  }
+
+  // Clear url params once we have the Google ID token
+  // Be sure to use the redirect uri route
+  router.replace('/', undefined, { shallow: true });
+}, [router]);
+
+/**
+ * Mint a new PKP for the authorized Google account
+ */
+async function mint() {
+  setView(Views.MINTING);
+
+  try {
+    // Mint new PKP
+    const newPKP = await mintGooglePKP(googleIdToken);
+
+    // Add new PKP to list of PKPs
+    const morePKPs = pkps.push(newPKP);
+    setPKPs(morePKPs);
+
+    setView(Views.MINTED);
+    setView(Views.CREATING_SESSION);
+
+    // Get session sigs for new PKP
+    await createSession(newPKP);
+  } catch (err) {
+    setError(err);
+    setView(Views.ERROR);
+  }
+}
+
+/**
+ * Generate session sigs for current PKP
+ *
+ * @param {Object} PKP - PKP object
+ */
+async function createSession(pkp) {
+  setView(Views.CREATING_SESSION);
+
+  try {
+    // Connect to LitNodeClient if not already connected
+    if (!litNodeClient.ready) {
+      await litNodeClient.connect();
+    }
+
+    const authNeededCallback = async authCallbackParams => {
+      let chainId = 1;
+      try {
+        const chainInfo = ALL_LIT_CHAINS[authCallbackParams.chain];
+        chainId = chainInfo.chainId;
+      } catch {
+        // Do nothing
+      }
+
+      let response = await litNodeClient.signSessionKey({
+        authMethods: [
+          {
+            authMethodType: 6,
+            accessToken: googleIdToken,
+          },
+        ],
+        pkpPublicKey: pkp.publicKey,
+        expiration: authCallbackParams.expiration,
+        resources: authCallbackParams.resources,
+        chainId,
+      });
+
+      return response.authSig;
+    };
+
+    // Generate session sigs with the given session params
+    const sessionSigs = await litNodeClient.getSessionSigs({
+      chain: 'ethereum',
+      resources: [`litAction://*`],
+      authNeededCallback,
+    });
+
+    setCurrentPKP(pkp);
+    setSessionSigs(sessionSigs);
+
+    setView(Views.SESSION_CREATED);
+  } catch (err) {
+    setError(err);
+    setView(Views.ERROR);
+  }
+}
+
+/**
+ * Sign a message with current PKP
+ */
+async function signMessage() {
+  try {
+    const toSign = ethers.utils.arrayify(ethers.utils.hashMessage(message));
+    const litActionCode = `
+      const go = async () => {
+        // this requests a signature share from the Lit Node
+        // the signature share will be automatically returned in the response from the node
+        // and combined into a full signature by the LitJsSdk for you to use on the client
+        // all the params (toSign, publicKey, sigName) are passed in from the LitJsSdk.executeJs() function
+        const sigShare = await LitActions.signEcdsa({ toSign, publicKey, sigName });
+      };
+      go();
+    `;
+    // Sign message
+    const results = await litNodeClient.executeJs({
+      code: litActionCode,
+      sessionSigs,
+      jsParams: {
+        toSign: toSign,
+        publicKey: currentPKP.publicKey,
+        sigName: 'sig1',
+      },
+    });
+    // Get signature
+    const result = results.signatures['sig1'];
+    const signature = ethers.utils.joinSignature({
+      r: '0x' + result.r,
+      s: '0x' + result.s,
+      v: result.recid,
+    });
+    setSignature(signature);
+
+    // Get the address associated with the signature created by signing the message
+    const recoveredAddr = ethers.utils.verifyMessage(message, signature);
+    setRecoveredAddress(recoveredAddr);
+    // Check if the address associated with the signature is the same as the current PKP
+    const verified =
+      currentPKP.ethAddress.toLowerCase() === recoveredAddr.toLowerCase();
+    setVerified(verified);
+  } catch (err) {
+    setError(err);
+    setView(Views.ERROR);
+  }
+}
+
+useEffect(() => {
+  /**
+   * Initialize LitNodeClient
+   */
+  async function initLitNodeClient() {
+    try {
+      // Set up LitNodeClient
+      const litNodeClient = new LitNodeClient({
+        litNetwork: 'serrano',
+        debug: false,
+      });
+
+      // Connect to Lit nodes
+      await litNodeClient.connect();
+
+      // Set LitNodeClient
+      setLitNodeClient(litNodeClient);
+    } catch (err) {
+      setError(err);
+      setView(Views.ERROR);
+    }
+  }
+
+  if (!litNodeClient) {
+    initLitNodeClient();
+  }
+}, [litNodeClient]);
+
+useEffect(() => {
+  // Check if app has been redirected from Lit login server
+  if (isSignInRedirect(REDIRECT_URI)) {
+    handleRedirect();
+  }
+}, [handleRedirect]);
+
+
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js</title>
+        <meta
+          name="description"
+          content="Next.js template for building apps with Radix UI and Tailwind CSS"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
+        <div className="flex max-w-[980px] flex-col items-start gap-2">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+            Beautifully designed components <br className="hidden sm:inline" />
+            built with Radix UI and Tailwind CSS.
+          </h1>
+          <p className="max-w-[700px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
+            Accessible and customizable components that you can copy and paste
+            into your apps. Free. Open Source. And Next.js 13 Ready.
+          </p>
+        </div>
+        <div className="flex gap-4">
+          <Link
+            href={siteConfig.links.docs}
+            target="_blank"
+            rel="noreferrer"
+            className={buttonVariants({ size: "lg" })}
+          >
+            Documentation
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={buttonVariants({ variant: "outline", size: "lg" })}
+          >
+            GitHub
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+/**
+ * Redirect user to the Google authorization page
+ */
+function signInWithGoogle() {
+  // Get login url
+  const loginUrl = getLoginUrl(REDIRECT_URI);
+  // Redirect to login url
+  window.location.assign(loginUrl);
+}
+
+/**
+ * Fetch PKPs associated with the given Google account through the relay server
+ *
+ * @param {string} idToken - Google ID token
+ *
+ * @returns PKPs associated with Google account
+ */
+async function fetchGooglePKPs(idToken) {
+  // Fetch PKPs associated with Google OAuth
+  const body = JSON.stringify({
+    idToken: idToken,
+  });
+  const fetchRes = await fetchPKPs(body);
+  const { pkps } = fetchRes;
+  if (!pkps) {
+    throw new Error('Unable to fetch PKPs through relay server');
+  }
+  return pkps;
+}
+
+/**
+ * Mint a PKP for the given Google account through the relay server
+ *
+ * @param {string} idToken - Google ID token
+ *
+ * @returns newly minted PKP
+ */
+async function mintGooglePKP(idToken) {
+  // Mint a new PKP via relay server
+  const body = JSON.stringify({
+    idToken: idToken,
+  });
+  const mintRes = await mintPKP(body);
+  const { requestId } = mintRes;
+  if (!requestId) {
+    throw new Error('Unable to mint PKP through relay server');
+  }
+
+  // Poll for status of minting PKP
+  const pollRes = await pollRequestUntilTerminalState(requestId);
+  if (
+    !pollRes ||
+    !pollRes.pkpTokenId ||
+    !pollRes.pkpEthAddress ||
+    !pollRes.pkpPublicKey
+  ) {
+    throw new Error('Missing poll response or new PKP from relay server');
+  }
+  const newPKP = {
+    tokenId: pollRes.pkpTokenId,
+    ethAddress: pollRes.pkpEthAddress,
+    publicKey: pollRes.pkpPublicKey,
+  };
+  return newPKP;
+}
