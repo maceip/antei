2ee9387b05ef3d2ec4f2cd088209c4e1cc0f3801
rpm <ryan.macarthur@gmail.com>
2023-04-16 09:33:08 +0900

    judging

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,11 +1,10 @@
 import * as React from "react"
 import Link from "next/link"
-
+import { Button } from "@/components/ui/button"
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
-import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
